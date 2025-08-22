fb3b94abb9ca40e744815ae0bc10f2e6958e3963
github-bdem <5677735+github-bdem@users.noreply.github.com>
2025-08-22 16:55:13 -0400

    Adds localStorage Persistence

--- src/components/ReadingList/ReadingListContextProvider.tsx
+++ src/components/ReadingList/ReadingListContextProvider.tsx
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import {
     readingListReducer,
     initialReadingListState,
     ReadingListContext,
+    State,
 } from "./ReadingListContext";
 
 const ReadingListContextProvider: React.FC<{ children: React.ReactNode }> = ({
     children,
 }) => {
+    const existingReadingListState = localStorage.getItem("readingListState");
+
+    const applicableInitialState = existingReadingListState
+        ? (JSON.parse(existingReadingListState) as State)
+        : initialReadingListState;
+
     const [state, dispatch] = React.useReducer(
         readingListReducer,
-        initialReadingListState,
+        applicableInitialState,
     );
+
+    // updating localstorage when state updates
+    useEffect(() => {
+        if (state) {
+            localStorage.setItem("readingListState", JSON.stringify(state));
+        } else {
+            localStorage.removeItem("readingListState");
+        }
+    }, [state]);
 
     return (
         <ReadingListContext.Provider value={{ state, dispatch }}>
